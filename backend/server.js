4e1ee0de7e9a6391a025347949517c77c095a0f9
Juanelc4734k <jtoroblandon@gmail.com>
2024-04-30 21:50:49 -0500

    Cambios

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,16 @@
 const express = require('express');
-const mysql = require('mysql');
 const cors = require('cors');
-
 const app = express();
+const authRoutes = require('./routes/authRoutes')
+const db = require('./db');
+
+//Middlewares
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "bubblebox"
-});
+//Routes
+app.use('/auth', authRoutes);
 
-app.get('/users', (res, req) => {
-    const sql = "SELECT * FROM usuarios";
-    db.query(sql, (err, data) => {
-        if(err) return res.json(err);
-        return res.json(data);
-    })
-})
-
-app.get('/', (re, res) => {
-    return res.json("BackEnd Side");
-});
-
+//Puerto
 app.listen(8081, () => {
     console.log("Puerto en escucha")
 })
