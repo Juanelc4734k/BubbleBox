eab77cddf097cf1b561247e5f05988ba1c66c9c0
Juanelc4734k <jtoroblandon@gmail.com>
2024-05-02 23:01:17 -0500

    Carpeta CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './components/Login';
 import Register from './components/register';
+import ('./css/App.css');
 
 function App() {
   return (
