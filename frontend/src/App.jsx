b26ef4d1fc920ea9fbf479a7ddbe26bccbb93e84
Juan Andres Toro <jtoroblandon@gmail.com>
2024-05-27 19:54:05 -0500

    archivo .gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from './components/Login';
-import Register from './components/register';
+import Register from './components/Register';
 import Home from './components/Home';
 import ('./css/App.css');
 
