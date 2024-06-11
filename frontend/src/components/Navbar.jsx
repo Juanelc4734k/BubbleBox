f91779674fa75c13468b2c34c95751154d86d44e
Juanelc4734k <jtoroblandon@gmail.com>
2024-06-11 12:17:22 -0500

    Nav, Side, Footer

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><img src="" alt="Logo" /></li>
+                <li><a href="">Notificaciones</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
