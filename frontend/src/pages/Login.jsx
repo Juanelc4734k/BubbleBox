a2d73dec14184132e5efd08f522ff1bc89260066
Yeniii <poopii0129@gmail.com>
2024-08-30 17:30:23 -0500

    Se realizaron los cambios y estilos en el login, falta el icono animado

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import LoginForm from '../components/auth/LoginForm';
 
+
 const Login = ({ setIsAuthenticated }) => {
   const [message, setMessage] = useState('');
 
   return (
     <div>
-      <h1>Login</h1>
+      {/*<h1>Login</h1>*/}
       <LoginForm setMessage={setMessage} setIsAuthenticated={setIsAuthenticated} />
       {message && <p>{message}</p>}
     </div>
