22457ddbe3500f564c2bcd09a52b0d8b10424b08
Juan Andres <jtoroblandon@gmail.com>
2024-09-04 20:39:33 -0500

    feat(app): Sidebar para administradores y usuarios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,6 @@ import RecoverPass from './pages/RecoverPass';
 import RecoverPassPage from './pages/ResetPass';
 import RoleProtectedRoute from './components/auth/RoleProtectedRoute';
 import AdminDashboard from './dashboard/pages/AdminDashboard';
-import RoleProtectedRoute from './components/auth/RoleProtectedRoute';
-import AdminDashboard from './dashboard/pages/AdminDashboard';
 import './assets/css/layout/layout.css';
 
 export default function App() {
@@ -32,19 +30,6 @@ export default function App() {
   return (
     <Router>
       <div className='app'>
-        {isAuthenticated && <Sidebar setIsAuthenticated={setIsAuthenticated} />}
-        <Routes>
-          <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path='/friends' element={<ProtectedRoute><Friends /></ProtectedRoute>} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-          <Route path='/recover-password' element={<RecoverPass />} />
-          <Route path='/recuperar-contrasena' element={<RecoverPassPage />} />
-          <Route path='/users' element={<ProtectedRoute><Users /></ProtectedRoute>} />
-          <Route path='/perfil' element={<ProtectedRoute><Profiles /></ProtectedRoute>} />
-          <Route path='/perfil/:userId' element={<ProtectedRoute><Profiles /></ProtectedRoute>} />
-          <Route path='/admin/*' element={<RoleProtectedRoute allowedRoles={['administrador']}><AdminDashboard /></RoleProtectedRoute>} />
-        </Routes>
         {isAuthenticated && <Navbar />}
         <div className="layout">
           {isAuthenticated && <Sidebar setIsAuthenticated={setIsAuthenticated} />}
