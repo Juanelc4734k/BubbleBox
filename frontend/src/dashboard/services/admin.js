a461bf341e168b5b2ee205ee9f1591ab2c27b395
Juan Andres <jtoroblandon@gmail.com>
2024-09-09 02:01:49 -0500

    feat(app): endpoint getUsers añadido a dashboard

--- frontend/src/dashboard/services/admin.js
+++ frontend/src/dashboard/services/admin.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:3000';
 
+export const getUsers = async () => {
+    const response = await axios.get(`${API_URL}/users/usuarios`);
+    return response.data;
+}
+
 export const getUserById = async (id) => {
     const response = await axios.get(`${API_URL}/users/usuario/${id}`);
     return response.data;
