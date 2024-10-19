83cd264857566e8d13bdb6b14d418536c027ab80
Mariana Nisperuza <marinispe0311@gmail.com>
2024-10-20 00:17:35 +0200

    Estilos del formulario de registro

--- frontend/src/components/auth/RegisterForm.jsx
+++ frontend/src/components/auth/RegisterForm.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import { register } from '../../services/auth';
+import '../../assets/css/auth/resgistro.css';
+import Animation from '../../assets/images/icon_login.png';
+import iconoGoogle from '../../assets/images/cromo.png';
+import iconoFacebook from '../../assets/images/facebook.png';
+import iconoInstagram from '../../assets/images/instragram.png';
 
 export default function RegisterForm({ setMessage }) {
     const [formData, setFormData] = useState({ nombre: '', username: '', email: '', contraseña: '' });
@@ -19,13 +24,34 @@ export default function RegisterForm({ setMessage }) {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" name='nombre' placeholder='Nombre' value={formData.nombre} onChange={handleChange} required/>
-            <input type="text" name='username' placeholder='Username' value={formData.username} onChange={handleChange} required/>
-            <input type="email" name='email' placeholder='Correo' value={formData.email} onChange={handleChange} required/>
-            <input type="password" name='contraseña' placeholder='Contraseña' value={formData.contraseña} onChange={handleChange} required/>
-            <button type="submit">Registrate</button>
-        </form>
+
+        <div className="registerFrom">
+            <div className="bienvenidoRegister">
+                <h1>Bienvenido a BubbleBox</h1>
+                <img src={Animation} alt="" className="registerAnimation" />
+            </div>
+            <div className="FormularioRegister">
+                <form onSubmit={handleSubmit}>
+                    <h2>Registrate</h2>
+                    <label htmlFor="Nombre">Nombre</label>
+                    <input type="text" name='nombre' value={formData.nombre} onChange={handleChange} required/>
+                    <label htmlFor="Username">User name</label>
+                    <input type="text" name='username' value={formData.username} onChange={handleChange} required/>
+                    <label htmlFor="email">Correo</label>
+                    <input type="email" name='email' value={formData.email} onChange={handleChange} required/>
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" name='contraseña' value={formData.contraseña} onChange={handleChange} required/>
+                   <div className="iconos-register">
+                   <div className="icono"><img src={iconoGoogle} alt="Icono de Google"/></div>
+                        <div className="icono"><img src={iconoFacebook} alt="Icono de Facebook"/></div>
+                        <div className="icono"><img src={iconoInstagram} alt="Icono de Instagram"/></div>
+                   </div>
+                    <button type="submit">Registrate</button>
+                </form>
+            </div>
+           
+        </div>
+        
     );
 };
 
