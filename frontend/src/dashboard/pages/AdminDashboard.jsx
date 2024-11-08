8458139894772b60347970a5c534791298a45723
Juan Andres Toro Blandon <jtoroblandon@gmail.com>
2024-11-08 18:35:38 +0100

    Cambios front (comunidades y home)

--- frontend/src/dashboard/pages/AdminDashboard.jsx
+++ frontend/src/dashboard/pages/AdminDashboard.jsx
@@ -10,7 +10,12 @@ function AdminDashboard() {
             <Route path="/" element={<HomeAdmin />} />
             <Route path="/users" element={<Users />} />
             <Route path="/comunities" element={<h1>Comunidades</h1>} />
-            <Route path="/chats" element={<h1>Chats</h1>} />
+            <Route path="/posts" element={<h1>Posts</h1>} />
+            <Route path="/comments" element={<h1>Comments</h1>} />
+            <Route path="/reports" element={<h1>Reports</h1>} />
+            <Route path="/settings" element={<h1>Settings</h1>} />
+            <Route path="/profile" element={<h1>Profile</h1>} />
+            <Route path="/recover" element={<h1>Recover DB</h1>} />
         </Routes>
     </div>
   )
