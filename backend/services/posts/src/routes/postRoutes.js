295d8b0abb16d5ec850130871c44eae94eb8618d
Juan Andres Toro <jtoroblandon@gmail.com>
2025-01-27 14:20:32 -0500

    `Refactor postController.js and postRoutes.js to remove imagen from req.body and add multer middleware for image uploads`

--- backend/services/posts/src/routes/postRoutes.js
+++ backend/services/posts/src/routes/postRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
+const upload = require('../middleware/multer');
 
 router.get('/obtener-todos', postController.getAllPosts);
 router.get('/usuarios', postController.getUserPosts); //Añadir documentacion a obsidian
 router.get('/comunidades', postController.getCommunityPosts); //Añadir documentacion a obsidian
 router.get('/obtener/:id', postController.getPostById);
-router.post('/crear', postController.createPost);
-router.post('/crear-publicacion-comunidad', postController.crearPublicacionComunidad); //Añadir documentacion a obsidian
+router.post('/crear', upload.single('imagen'), postController.createPost, {});
+router.post('/crear-publicacion-comunidad', upload.single('imagen'), postController.crearPublicacionComunidad); //Añadir documentacion a obsidian
 router.put('/actualizar/:id', postController.updatePost);
 router.delete('/eliminar/:id', postController.deletePost);
 
